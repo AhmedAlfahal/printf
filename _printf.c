a0d0b5652d0fcf844c1454a44581d6184d2ecf8d
AhmedAlfahal <ahmednader02@gmail.com>
2023-10-17 12:14:44 -0400

    modifying it

--- _printf.c
+++ _printf.c
@@ -4,13 +4,13 @@
 /**
 *	_printf - memic the funtion from the standard library (printf)
 *
-*	@s1: char *
+*	@format: char *
 *
 *	Return: how many characters that printed
 **/
 
 
-int	_printf(const char *s1, ...)
+int _printf(const char *format, ...)
 {
 	va_list	mylist;
 	int		i;
@@ -18,14 +18,14 @@ int	_printf(const char *s1, ...)
 
 	i = 0;
 	counter = 0;
-	va_start(mylist, s1);
-	while (s1[i] != 0)
+	va_start(mylist, format);
+	while (format[i] != 0)
 	{
-		if (s1[i] != '%')
-			counter += _printf_putchr(s1[i]);
-		if (s1[i++] == '%')
+		if (format[i] != '%')
+			counter += _printf_putchr(format[i]);
+		if (format[i++] == '%')
 		{
-			counter += _printing_detection(s1[i], mylist);
+			counter += _printing_detection(format[i], mylist);
 			i++;
 		}
 	}
--- main.h
+++ main.h
@@ -1,10 +1,10 @@
-#ifndef PRINTF_H
-#define PRINTF_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdarg.h>
 #include <unistd.h>
 
-int	_printf(const char *s1, ...);
+int _printf(const char *format, ...);
 int	_printf_putchr(char chr);
 int	_putstr(char *str);
 int	_printing_detection(char s, va_list mylist);
